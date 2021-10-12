03338c45db1986b7240ef31c13515d8ff1f14e74
vicenteyah <vicente.yah.dzul@icloud.com>
2021-10-12 14:12:08 -0500

    twitter button was added to layout internal

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer'
-
+import TwittButton from '../components/TwitterButton'
 const Layout = (props) => (
     <>
         <Header/>
+        <TwittButton/>
         {props.children}
         <Footer/>
     </>
