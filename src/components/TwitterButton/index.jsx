f4a3d22953c19760583c235d3d390636a2b01fed
vicenteyah <vicente.yah.dzul@icloud.com>
2021-10-12 14:12:46 -0500

    twitter button as component was created

---
+++ src/components/TwitterButton/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import twitter from '../../img/twitter.svg'
+import './styles.css'
+
+const TwittButton = () => (
+    <div class="social-twitter">
+        <a href="/"><img src={twitter} alt=""/></a>
+    </div>
+)
+
+export default TwittButton
